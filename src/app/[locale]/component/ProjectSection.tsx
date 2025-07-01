37993996246d56db5dbaba1f6eefb8dacefb8b92
quantum-builds <muhammadmassab.dev@gmail.com>
2025-07-01 18:41:40 +0500

    fixed: scroll margins

--- src/app/[locale]/component/ProjectSection.tsx
+++ src/app/[locale]/component/ProjectSection.tsx
@@ -34,7 +34,7 @@ export default function ProjectSection() {
   return (
     <section
       id="projects"
-      className="scroll-mt-28 w-4/5 mx-auto min-h-[900px] lg:min-h-[600px] lg:h-[650px] xl:h-[740px] 2xl:h-[800px] flex flex-col justify-between  py-12 font-inter"
+      className="scroll-mt-20 w-4/5 mx-auto min-h-[900px] lg:min-h-[600px] lg:h-[650px] xl:h-[740px] 2xl:h-[820px] flex flex-col justify-between  py-12 font-inter"
     >
       <div className="w-full flex flex-col items-center justify-center gap-4 sm:gap-5 md:gap-6 lg:gap-8 ">
         <motion.h2
